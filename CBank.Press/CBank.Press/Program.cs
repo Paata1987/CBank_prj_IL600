691f0b657c66be76a0e818b775a637730a7ec5eb
unknown <paatatepnadze1@gmail.com>
2022-10-21 09:41:01 +0200

    creating TO DO loop

--- CBank.Press/CBank.Press/Program.cs
+++ CBank.Press/CBank.Press/Program.cs
@@ -24,7 +24,43 @@
         //check username and password
         if (userName =="system" && password =="manager")
         {
-            System.Console.WriteLine("TO DO: Main menu here");
+            //declare variable to menu choice
+            int mainMenuChoice = -1;
+            do
+            {
+                //show main menu
+                System.Console.WriteLine("\n:::Main menu:::");
+                System.Console.WriteLine("1. Customers ");
+                System.Console.WriteLine("2. Acounte ");
+                System.Console.WriteLine("3. Funds Transfer ");
+                System.Console.WriteLine("4. Funds Transfer Statements ");
+                System.Console.WriteLine("5. Account Statements ");
+                System.Console.WriteLine("0. Exit ");
+
+                //Accept menu choice from key
+                System.Console.Write("Enter choice ");
+                mainMenuChoice = int.Parse(System.Console.ReadLine());
+
+                //switch-case to check menu choice
+                switch (mainMenuChoice)
+                {
+                    case 1: //TO DO : Display Customers menu
+                        break;
+                    case 2: //TO DO : Display Acounte menu
+                        break;
+                    case 3: //TO DO : Display Transfer menu
+                        break;
+                    case 4: //TO DO : Display Transfer Statements menu
+                        break;
+                    case 5: //TO DO : Display Account Statements menu
+                        break;
+                }
+            } while (mainMenuChoice != 0);
+
+        }
+        else
+        {
+            System.Console.WriteLine("Invalid Username  or  password");
         }
 
         //about exit
